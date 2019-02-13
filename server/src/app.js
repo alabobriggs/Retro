ae045c42e0c84e0d114a29cdab4bdd0daafd8acd
alabobriggs <alabobriggs@gmail.com>
2019-02-13 12:54:54 +0100

    add cors support

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,17 @@ const app = express()
 
 // SETUPS =====================================================
 
+// set hearders to allow CORS (Cross Origin Resource Sharing)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200)
+    }
+    next()
+})
+
 // graphql
 app.use('/graphql', graphqlHTTP({
     schema,
